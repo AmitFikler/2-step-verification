25280fdbaf4c992213970cd0af6c67afb3b0e754
AmitFikler <amitfikler@gmail.com>
2021-12-12 16:27:28 +0200

    add twofactor api for 2 step verification

--- back/index.js
+++ back/index.js
@@ -8,11 +8,29 @@ const PORT = 3001;
 app.use(express.json());
 
 app.post('/verification', (req, res) => {
-  const { username } = req.params;
-  res.send(twofactor.generateSecret({ name: 'Fikler App', account: username }));
+  const { username } = req.body;
+  if (username) {
+    res.send(
+      twofactor.generateSecret({ name: 'Fikler App', account: username })
+    );
+  } else {
+    res.send('username is missing');
+  }
 });
 
-app.get('/', (req, res) => {});
+app.put('/verificationToken', (req, res) => {
+  const { secret, token } = req.body;
+  const checkToken = twofactor.verifyToken(secret, token);
+  if (checkToken.delta === 0) {
+    res.send('verify token');
+  } else {
+    res.send('invalid token');
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('hello!');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
